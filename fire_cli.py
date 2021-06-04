aca8f586ccac8c31f308d9c599b1048b6e331290
GL <1404919041@qq.com>
2021-06-04 10:31:33 +0800

    use fire to build cli

---
+++ fire_cli.py
@@ -0,0 +1,16 @@
+import fire
+
+# code
+# functions or classes
+
+if __name__ == "__main__":
+    fire.Fire()
+
+# command arguments
+# --title="this is title"
+
+# str
+# '"str"'
+
+# dic
+# '{"key":"val"}'

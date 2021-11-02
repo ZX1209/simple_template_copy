c9a27de8dd82dfdb6307a47a945fb0e3cfb390de
GL <1404919041@qq.com>
2021-11-02 14:02:51 +0800

    try use flit init

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["fuzzywuzzy"],  # Optional
+    install_requires=["fuzzywuzzy", "docopt"],  # Optional
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
--- src/docopt-test.py
+++ src/docopt-test.py
@@ -7,7 +7,7 @@ from docopt import docopt
 import logging as log
 import shutil
 import sys
-from simple_template_copy import core  # need install simple_template_copy
+
 
 doc = """simple template copy by python
 
@@ -29,16 +29,12 @@ Docs:
     cmd.py <template_str> : search template by template_str
     cmd.py <template_str> <target_str> : complate copy
 
-Attention:
-    attention to file cover
 """
 test_cmd = """
 
 
 
 """
-__doc__ = doc
-__version__ = "0.2"
 
 
 def main():
@@ -48,18 +44,10 @@ def main():
     # set log level
     log_map = {"debug": log.DEBUG, "info": log.INFO, "warn": log.WARN}
     log_level = log_map.get(arguments.get("--log_level").lower())
-
     log.basicConfig(level=log_level)
     # check arguments
     log.debug(arguments)
 
-    core.CopyTemplate(
-        arguments.get("<template_str>"),
-        arguments.get("<target_strs>"),
-        log_level=log_level,
-        help_message=doc,
-    )
-
 
 if __name__ == "__main__":
-    main()
+    print(main())
